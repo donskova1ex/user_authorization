f147f66ad3c91b9ea0ceb6a4e97451ae02a6fd01
Donskov <donskov_a@icloud.com>
2024-12-26 08:19:21 +0500

    new errors create. Domain premission creating

--- internal/domain/permission.go
+++ internal/domain/permission.go
@@ -1,1 +1,7 @@
 package domain
+
+type Permission struct {
+	ID   uint32 `json:"id" db:"id"`
+	UUID string `json:"uuid" db:"uuid"`
+	Name string `json:"name" db:"name"`
+}
--- internal/errors.go
+++ internal/errors.go
@@ -1,1 +1,6 @@
 package internal
+
+import "errors"
+
+var QueryError = errors.New("db query error")
+var ErrorRowScan = errors.New("db row scan error")
