21520fdbf4c706422a0c01f64ad0933075f742c1
Donskov <donskov_a@icloud.com>
2024-12-26 16:38:49 +0500

    create user struct

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,1 +1,8 @@
 package domain
+
+type User struct {
+	ID           uint32 `json:"id" db:"id"`
+	UUID         string `json:"uuid" db:"uuid"`
+	Name         string `json:"name" db:"name"`
+	PasswordHash string `json:"password_hash" db:"password_hash"`
+}
