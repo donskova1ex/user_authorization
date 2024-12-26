6bca5af583f671566df9da6493388771b38e7895
Donskov <donskov_a@icloud.com>
2024-12-26 13:41:25 +0500

    permissions repository create

--- internal/repositories/permissions.go
+++ internal/repositories/permissions.go
@@ -2,6 +2,9 @@ package repositories
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"fmt"
 	"github.com/donskova1ex/user_authorization/internal"
 	"github.com/donskova1ex/user_authorization/internal/domain"
 	"github.com/google/uuid"
@@ -36,4 +39,52 @@ func (p *PermissionsProstgres) CreatePermission(ctx context.Context, permission 
 		ID:   id,
 	}
 	return newPermission, nil
+}
+
+func (p *PermissionsProstgres) PermissionsAll(ctx context.Context) ([]*domain.Permission, error) {
+	var permissions []*domain.Permission
+	err := p.db.SelectContext(ctx, &permissions, "SELECT id, uuid, name from permissions")
+	if errors.Is(err, sql.ErrNoRows) {
+		return permissions, nil
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("cannot get permissions: %w", internal.ErrReadRows)
+	}
+	return permissions, nil
+}
+
+func (p *PermissionsProstgres) PermissionByUUID(ctx context.Context, uuid string) (*domain.Permission, error) {
+	var permission = &domain.Permission{}
+	query := "SELECT id, uuid, name FROM permissions WHERE uuid=$1"
+	err := p.db.GetContext(ctx, &permission, query, uuid)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, fmt.Errorf("%w with uuid [%s]", internal.ErrNotFound, uuid)
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("%w with uuid [%s]", internal.ErrReadRows, uuid)
+	}
+	return permission, nil
+}
+
+func (p *PermissionsProstgres) DeletePermission(ctx context.Context, uuid string) error {
+	query := "DELETE FROM permissions WHERE uuid=$1"
+	_, err := p.db.ExecContext(ctx, query, uuid)
+	if errors.Is(err, sql.ErrNoRows) {
+		return fmt.Errorf("%w with uuid [%s]", internal.ErrNotFound, uuid)
+	}
+	if err != nil {
+		return fmt.Errorf("%w with uuid [%s]", internal.ErrNotDelete, uuid)
+	}
+	return nil
+}
+
+func (p *PermissionsProstgres) UpdatePermission(ctx context.Context, permission *domain.Permission) (*domain.Permission, error) {
+	query := "UPDATE permissions SET name=$1 WHERE uuid=$2"
+	_, err := p.db.ExecContext(ctx, query, permission.Name, permission.UUID)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, fmt.Errorf("%w with uuid [%s]", internal.ErrNotFound, permission.UUID)
+	}
+	return permission, nil
 }
