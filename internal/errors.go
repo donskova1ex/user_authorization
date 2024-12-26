40c1ec201c3c21b83a9d57d2facdf9a2c346b89f
Donskov <donskov_a@icloud.com>
2024-12-26 13:40:50 +0500

    new errors create.

--- internal/errors.go
+++ internal/errors.go
@@ -4,3 +4,6 @@ import "errors"
 
 var QueryError = errors.New("db query error")
 var ErrorRowScan = errors.New("db row scan error")
+var ErrReadRows = errors.New("can not read rows")
+var ErrNotFound = errors.New("entity not found")
+var ErrNotDelete = errors.New("entity not delete")
